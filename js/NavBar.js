2a3afa7042a953d70e7bf22dddda43d3e020ee63
Sina Sadeghi <45234211+sinasadeghi83@users.noreply.github.com>
2020-03-20 01:05:43 +0330

    Add 3 main section of theme(static)
    Also fix navbar and round info

--- js/NavBar.js
+++ js/NavBar.js
@@ -3,12 +3,12 @@ function settingUpNavbar(){
   $("#navtitle").text(messages["navbarTitle"]);
   getData('./datas/basic/navbar.json', function(data, status, xhr){
     for(i = 0; i < data.links.length; i++){
-      var link = $('<a href="'+data.links[i].url+'" class="w3-bar-item w3-button w3-medium"></a>').text(messages[data.links[i].title]);
-      if(data.links[i].mobileView){
-        $('#mobilemenu').append(link);
-        link.className += ' w3-hide-small';
-      }
+      var link = $('<a href="'+data.links[i].url+'" class="w3-bar-item w3-button w3-medium'+(data.links[i].mobileView ? ' w3-hide-small' : '')+'"></a>').text(messages[data.links[i].title]);
       $("#mobilebutton").before(link);
+      if(data.links[i].mobileView){
+        link = $('<a href="'+data.links[i].url+'" class="w3-bar-item w3-button w3-medium"></a>').text(messages[data.links[i].title]);
+        $('#mobilemenu').append(link);
+      }
     }
   },function(jqXhr, textStatus, errorMessage){
     $("#mobilebutton").before("Error: " + errorMessage);
